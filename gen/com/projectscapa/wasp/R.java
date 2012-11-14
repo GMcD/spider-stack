85d32f47cf109115ead5dfc5d359e2b3a7d20645
Gary MacDonald <gary.macdonald@socialandviral.co.uk>
2012-11-14 16:19:28 +0000

    Loading progress bar, application page, Scapa icon.

--- gen/com/projectscapa/wasp/R.java
+++ gen/com/projectscapa/wasp/R.java
@@ -11,11 +11,21 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int avatar=0x7f020000;
+        public static final int chorizos=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int lambliver=0x7f020003;
+        public static final int lambsheart=0x7f020004;
+        public static final int ribeyesteak=0x7f020005;
+        public static final int saveloys=0x7f020006;
+        public static final int splash=0x7f020007;
+    }
+    public static final class id {
+        public static final int progressBar1=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int progress=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
