e432532a801a32fa7eb361bdf8ab1fe5616d7cc6
Gary MacDonald <gary.macdonald@socialandviral.co.uk>
2012-11-18 15:13:32 +0000

    SubModuled Android Assets, and siblinged Droid project.

--- droid/gen/com/projectscapa/wasp/R.java
+++ droid/gen/com/projectscapa/wasp/R.java
@@ -12,13 +12,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int avatar=0x7f020000;
-        public static final int chorizos=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int lambliver=0x7f020003;
-        public static final int lambsheart=0x7f020004;
-        public static final int ribeyesteak=0x7f020005;
-        public static final int saveloys=0x7f020006;
-        public static final int splash=0x7f020007;
+        public static final int ic_launcher=0x7f020001;
+        public static final int splash=0x7f020002;
     }
     public static final class id {
         public static final int progressBar1=0x7f060000;
